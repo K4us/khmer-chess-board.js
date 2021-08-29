d4ccea0a7dc92d3135700e855c72d248d725ef19
raksa <eng.raksa@gmail.com>
2021-08-28 21:04:21 -0500

    improve

--- dist/src/helpers/addCssNote.d.ts
+++
@@ -1,6 +0,0 @@
-import OptionsManager from '../OptionsManager';
-export default function addCssNote({ uniqueClassName, options, isEnglish }: {
-    uniqueClassName: string;
-    options: OptionsManager;
-    isEnglish?: boolean;
-}): void;
