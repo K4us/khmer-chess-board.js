b36ae9ba08addd0c9f3b68bd04814357ae822dbd
kforus <k4us.net@gmail.com>
2021-09-18 22:16:53 -0500

    release 1.0.47

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 export { default as KhmerChessBoard } from './KhmerChessBoard';
+export { BoardStatusEvent } from './event/BoardStatusEvent';
+export { default as MoveData } from './MoveData';
+export { default as BoardManager } from './BoardManager';
+export { default as OptionsManager } from './OptionsManager';
+export { default as SoundManager } from './SoundManager';
+export { default as CellManager } from './CellManager';
+export { default as PieceShadowManager } from './PieceShadowManager';
+export { default as GraveyardManager } from './GraveyardManager';
+export { default as MessageManager } from './MessageManager';
+export { default as PlayManager } from './PlayManager';
+
 export * from 'khmer-chess';
 import { KhmerChess } from 'khmer-chess';
 import KhmerChessBoard from './KhmerChessBoard';
